f33c2e359e4c95560a5aa3cc7b1718f8d2c3a1ae
SviatoslavPotapenko <Sviatoslav.Potapenko@lnu.edu.ua>
2025-11-18 22:42:02 +0200

    add solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -3,8 +3,24 @@
 /***
  * @return {function}
  */
-function makeInfinityAdder() {
-  // write code here
+function makeInfinityAdder(num1 = 0) {
+  let sum = num1;
+  let res = 0;
+
+  const adder = (num2) => {
+    if (num2 === undefined) {
+      res = sum;
+      sum = 0;
+
+      return res;
+    } else {
+      sum += num2;
+
+      return adder;
+    }
+  };
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
